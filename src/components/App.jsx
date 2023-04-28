c864476e7eaa879d147070d4cd001ae70ca7feda
Danil Deineka <tripledpadik@gmail.com>
2023-04-28 23:40:57 +0300

    Make profile and stat blocks

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,3 +1,10 @@
+import user from './Profile/user.json'; 
+import data from './Statistics/data.json';
+import {Profile} from './Profile/Profile'
+import {Statistics} from './Statistics/Statistics';
+import {FriendList} from './FriendList/FriendList'
+
+
 export const App = () => {
   return (
     <div
@@ -10,7 +17,11 @@ export const App = () => {
         color: '#010101'
       }}
     >
-      React homework template
+      <Profile username={user.username} tag={user.tag} location={user.location} avatar={user.avatar} stats={user.stats}></Profile>
+
+      <Statistics title={"Upload stats"} stats={data}></Statistics>
+
+      <FriendList></FriendList>
     </div>
   );
 };
